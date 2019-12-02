2eb8f3a3bd3cb7ca0c625317bac2bdecdedc8a80
Joonas Nätynki <joonas.natynki@gmail.com>
2019-12-02 09:42:21 +0200

    Work

--- Source/COV/Private/COVCameraComponent.cpp
+++ Source/COV/Private/COVCameraComponent.cpp
@@ -3,6 +3,10 @@
 
 #include "COVCameraComponent.h"
 #include <GameFramework/Pawn.h>
+#include <Kismet/KismetMathLibrary.h>
+#include "UE4Helpers.h"
+
+DEFINE_LOG_CATEGORY(LogCamera)
 
 UCOVCameraComponent::UCOVCameraComponent()
 {
@@ -17,8 +21,37 @@ class APawn* UCOVCameraComponent::GetOwnerPawn() const
 	return ownerPawn;
 }
 
+void UCOVCameraComponent::UpdateCameraTransform(const float deltaTime)
+{
+	//	Position
+	FVector playerLocation = GetOwner()->GetActorLocation();
+	FVector cameraLocationAfterOffset = playerLocation + CameraPlayerDefaultOffset;
+	//	Now add in the rotation location offset
+	cameraLocationAfterOffset = UE4CodeHelpers::RotateVectorAroundPoint(cameraLocationAfterOffset, playerLocation, RotationOffset);
+
+	FVector currentCameraLocation = GetComponentLocation();
+	FVector finalCameraLocationResult = UKismetMathLibrary::VLerp(currentCameraLocation, cameraLocationAfterOffset, CameraTransformLerpSpeed);
+	SetWorldLocation(finalCameraLocationResult);
+
+	//	Rotation
+	FRotator playerRotation = GetOwner()->GetActorRotation();
+	FRotator cameraCurrentRotation = GetComponentRotation();
+	FRotator cameraEndRotation = UKismetMathLibrary::FindLookAtRotation(finalCameraLocationResult, playerLocation);
+
+	SetWorldRotation(cameraEndRotation);
+}
+
+void UCOVCameraComponent::RotateCamera(const ECameraRotation direction)
+{
+	FRotator rot = direction == (ECameraRotation::Clockwise) ? (FRotator(0.0f, DegreesPerRotation, 0.0f)) : (FRotator(0.0f, -DegreesPerRotation, 0.0f));
+	RotationOffset = RotationOffset + rot;
+}
+
 void UCOVCameraComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
+	UpdateCameraTransform(DeltaTime);
+
+	UE_LOG(LogCamera, Log, TEXT("%s"), (bIsAnimating)?(TEXT("TRUE")):(TEXT("FALSE")));
 }
--- Source/COV/Public/COVCameraComponent.h
+++ Source/COV/Public/COVCameraComponent.h
@@ -6,6 +6,14 @@
 #include "CinematicCamera/Public/CineCameraComponent.h"
 #include "COVCameraComponent.generated.h"
 
+UENUM(BlueprintType) enum ECameraRotation
+{
+	Clockwise,
+	CounterClockwise
+};
+
+DECLARE_LOG_CATEGORY_EXTERN(LogCamera, Log, All)
+
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent), Blueprintable)
 class COV_API UCOVCameraComponent : public UCineCameraComponent
 {
@@ -19,4 +27,19 @@ public:
 		class APawn* GetOwnerPawn() const;
 
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION(Category = "COVCamera", BlueprintCallable)
+		void RotateCamera(const ECameraRotation direction);
+
+private:
+
+	FORCEINLINE void UpdateCameraTransform(const float deltaTime);
+
+	float CameraTransformLerpSpeed = 0.08f;
+	const FVector CameraPlayerDefaultOffset = FVector(0, 1000.0f, 1000.0f);
+	bool bStepRotation = true;
+	float DegreesPerRotation = 45.0f;
+	FRotator RotationOffset = FRotator(0, 0, 0);
+	float RotationLerpSpeed = 0.08f;
+	bool bIsAnimating = false;
 };

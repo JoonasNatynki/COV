b0637b13387f13c43a548dbc2ab78caf9ebe640b
Joonas Nätynki <joonas.natynki@gmail.com>
2020-05-25 20:51:03 +0300

    Worked on the focus object UI and the interface that goes with it

---
+++ Source/COV/Private/InGameObjectProperties.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InGameObjectProperties.h"
+
+// Add default functionality here for any IInGameObjectProperties functions that are not pure virtual.

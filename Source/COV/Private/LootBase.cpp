acf7a6bc4200374019bc53dd11ee9af723c21333
Joonas Nätynki <joonas.natynki@gmail.com>
2020-12-31 18:14:45 +0200

    Added LootBase class

--- Source/COV/Private/LootBase.cpp
+++ Source/COV/Private/LootBase.cpp
@@ -1,22 +1,22 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "LootSpawner.h"
+#include "ALootBase.h"
 
 // Sets default values
-ALootSpawner::ALootSpawner()
+ALootBase::ALootBase()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 }
 
 // Called when the game starts or when spawned
-void ALootSpawner::BeginPlay()
+void ALootBase::BeginPlay()
 {
 	Super::BeginPlay();	
 }
 
 // Called every frame
-void ALootSpawner::Tick(float DeltaTime)
+void ALootBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 }
--- Source/COV/Public/LootSpawner.h
+++ Source/COV/Public/LootSpawner.h
@@ -6,6 +6,24 @@
 #include "GameFramework/Actor.h"
 #include "LootSpawner.generated.h"
 
+//	Loot definition and probability
+USTRUCT(BlueprintType)
+struct FLootSpawnDefinition
+{
+	GENERATED_BODY()
+
+public:	
+};
+
+//	The overall profile of a loot profile to be spawned through a loot spawned
+USTRUCT(BlueprintType)
+struct FLootProfile
+{
+	GENERATED_BODY()
+
+public:
+};
+
 UCLASS()
 class COV_API ALootSpawner : public AActor
 {
@@ -15,6 +33,12 @@ public:
 	// Sets default values for this actor's properties
 	ALootSpawner();
 
+	UPROPERTY(Category = "Loot", BlueprintReadWrite, VisibleAnywhere, meta = (ClampMin = 0, ClampMax = 100, ExposeOnSpawn = true))
+	int32 AmountToSpawn = 0;
+
+	UPROPERTY(Category = "Loot", BlueprintReadWrite, VisibleAnywhere, meta = (ExposeOnSpawn=true))
+	FLootProfile LootProfile;
+	
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
